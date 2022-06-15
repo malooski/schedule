09125af2717b5c7a768009c3798e75a3ee7d34f2
Malooski <malooski@protonmail.com>
2022-06-15 09:54:44 -0500

    Remove huge files.

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,27 +1,13 @@
 import { CSSProperties } from "react";
 
-import phasmoLogoUrl from "./assets/logos/Phasmo Logo.png";
-import phasmoBgUrl from "./assets/backgrounds/Phasmo Hero.jpg";
 import fortniteLogoUrl from "./assets/logos/Fortnite Logo.png";
-import fortniteBgUrl from "./assets/backgrounds/Fortnite Gameplay.mp4";
+import phasmoLogoUrl from "./assets/logos/Phasmo Logo.png";
 
 interface LogoMetadata {
     url: string;
 
     css?: CSSProperties;
 }
-
-interface EntryVideoBackground {
-    type: "video";
-    url: string;
-}
-
-interface EntryImageBackground {
-    type: "image";
-    url: string;
-}
-
-type EntryBackground = EntryVideoBackground | EntryImageBackground;
 
 const PHASMO_LOGO: LogoMetadata = {
     url: phasmoLogoUrl,
@@ -37,23 +23,13 @@ const FORTNITE_LOGO: LogoMetadata = {
     },
 };
 
-const FORTNITE_BACKGROUND: EntryVideoBackground = {
-    type: "video",
-    url: fortniteBgUrl,
-};
-
-const PHASMO_BACKGROUND: EntryImageBackground = {
-    type: "image",
-    url: phasmoBgUrl,
-};
-
 export interface ManifestScheduleEntry {
     title: string;
+
     time: string; // HH:MM
     date: string; // MM/DD
 
     logo?: LogoMetadata;
-    background?: EntryBackground;
 
     color?: string;
 }
@@ -76,14 +52,12 @@ export const MANIFEST: Manifest = {
             date: "06/15",
             time: "6:30pm",
             logo: PHASMO_LOGO,
-            background: PHASMO_BACKGROUND,
         },
         {
             title: "Fortnite",
             date: "06/16",
             time: "6:30pm",
             logo: FORTNITE_LOGO,
-            background: FORTNITE_BACKGROUND,
         },
     ],
 };
